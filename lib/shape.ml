bcdd167af5f7021c7fb9599e07fa8b287ce72699
Nangah Amandine Chi <nangahamandine@Nangah-Amandine.localdomain>
2023-10-06 12:09:28 +0100

    Updated code with some of the changes requested why awaiting further requirements

--- lib/shape.ml
+++ lib/shape.ml
@@ -72,19 +72,3 @@ let init () =
 let close () =
   ignore (read_line ());
   close_graph ()
-
-  let () =
-  init ();
-  set_color black;
-
-  (* Create shapes *)
-  let r1 = rectangle 200 100 in
-  let r2 = translate 100 0 r1 in
-  let c = circle 100 in
-  let e = ellipse 40 20 in
-  let t = triangle ~x1:50 ~y1:50 ~x2:100 ~y2:150 ~x3:150 ~y3:50 () in
-
-  (* Display shapes *)
-  show [r1; r2; c; e; t];
-
-  close ()
