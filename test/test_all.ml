0631f7bac282b40dee2edf77640e7e411ff93df4
kushalpokharel <kusaalpokharel@gmail.com>
2023-10-10 11:33:11 +0545

    Make the test simpler using translate

--- test/test_all.ml
+++ test/test_all.ml
@@ -1,2 +1,3 @@
 let () =
-  Test_circle.run ()
+  Test_circle.run ();
+  Test_circle_row.run ();
--- test/test_circle_row.ml
+++ test/test_circle_row.ml
@@ -1,22 +1,10 @@
 open Joy.Shape
 
-let row_of_circles dim_x dim_y spacing =
-  let circles = ref [] in
-  let total_width = 3 * spacing in 
-  let (center_x, center_y) = (dim_x / 2, dim_y / 2) in
-  let start_x = center_x - total_width / 2 in 
-  for i = 0 to 2 do
-    let x = start_x + i * spacing in
-    circles := circle ~x:x ~y:center_y 50 :: !circles
-  done;
-  !circles
-
-let () =
-  let (dim_x, dim_y) = (1000,1000) in
-  set_dimensions dim_x dim_y;
+let run ()=
   init ();
-  let spacing = 120 in 
-  let circles = row_of_circles dim_x dim_y spacing in
-  show circles;
+  let base_circle = circle 50 in
+  let circle1 = translate 150 0 base_circle in
+  let circle2 = translate (-150) 0 base_circle in
+  show [base_circle; circle1; circle2];
   ignore (read_line ());
   close()
