b5fa3def6826ec14fca8ed4fefebe1957f50b052
Nangah Amandine <amandinenangah1@gmail.com>
2023-10-10 01:10:25 +0100

    Add transformation: translate (#20)
    * Implementation of a translate transformation for a circle, rectangle, ellipse and triangle
    
    * Updated code with some of the changes requested why awaiting further requirements
    
    * Removed triangle transformations and updated the transformation files
    
    * Updated the translate transformation files to display the respective transformations
    
    * Updated dimensions
    
    * resolved conflicts
    
    * run dune-fmt
    
    ---------
    
    Co-authored-by: Nangah Amandine Chi <nangahamandine@Nangah-Amandine.localdomain>
    Co-authored-by: Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>

--- lib/shape.mli
+++ lib/shape.mli
@@ -5,6 +5,7 @@ val render_shape : shape -> unit
 val circle : ?x:int -> ?y:int -> int -> shape
 val rectangle : ?x:int -> ?y:int -> int -> int -> shape
 val ellipse : ?x:int -> ?y:int -> int -> int -> shape
+val translate: int -> int -> shape -> shape
 val show : shape list -> unit
 
 val draw_axes : bool -> unit
