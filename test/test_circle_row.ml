db2edacbcfb1b686c284e672de782c8b024d747e
kushalpokharel <kusaalpokharel@gmail.com>
2023-10-10 22:10:52 +0545

    Remove ignore readline

--- test/test_circle_row.ml
+++ test/test_circle_row.ml
@@ -6,5 +6,4 @@ let run ()=
   let circle1 = translate 150 0 base_circle in
   let circle2 = translate (-150) 0 base_circle in
   show [base_circle; circle1; circle2];
-  ignore (read_line ());
   close()
