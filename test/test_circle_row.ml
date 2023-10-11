8b7600dc7cca4b75f40d74a6ec5c6577498f9c71
kushalpokharel <kusaalpokharel@gmail.com>
2023-10-11 23:04:27 +0545

    Remove test file

--- test/test_circle_row.ml
+++
@@ -1,9 +0,0 @@
-open Joy.Shape
-
-let run ()=
-  init ();
-  let base_circle = circle 50 in
-  let circle1 = translate 150 0 base_circle in
-  let circle2 = translate (-150) 0 base_circle in
-  show [base_circle; circle1; circle2];
-  close()
