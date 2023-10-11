4aabcff1dfac465e77459a99f51cbfaa07a1896d
Fay Carsons <faycarsons23@gmail.com>
2023-10-11 10:32:24 -0400

    removed point and rot from shape.mli

--- lib/shape.mli
+++ lib/shape.mli
@@ -1,8 +1,6 @@
 type shape
 type shapes = shape list
 
-type point
-
 val render_shape : shape -> unit
 val circle : ?x:int -> ?y:int -> int -> shape
 val rectangle : ?x:int -> ?y:int -> int -> int -> shape
@@ -10,7 +8,6 @@ val ellipse : ?x:int -> ?y:int -> int -> int -> shape
 val translate: int -> int -> shape -> shape
 val show : shape list -> unit
 
-val rot : point -> int -> point 
 val rotate : int -> shape -> shape
 
 val draw_axes : bool -> unit
