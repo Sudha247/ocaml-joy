d123473c6a03e69e8018601fb277448b67f7ee02
joanita-51 <nakityojoanita51@gmail.com>
2023-10-14 22:23:56 +0300

    pentagon points

--- examples/pentagon.ml
+++ examples/pentagon.ml
@@ -1,45 +1,24 @@
 open Graphics
 
-type point = { x : int; y : int }
-type pentagon = { c : point; side_length : int}
-
-type shape = Pentagon of pentagon
-
-let canvas_size = (500, 500)
-let canvas_mid = { x = fst canvas_size / 2; y = snd canvas_size / 2 }
-
-let render_shape s =
-  match s with
-  | Pentagon pent ->
-      let side = pent.side_length in
-      let x = pent.c.x in
-      let y = pent.c.y in
-      let points =
-        [|
-          (x, y + side);
-          (x - side, y);
-          (x - (side/2), y - side);
-          (x + (side/2), y - side);
-          (x + side, y);
-        |]
-      in
-      draw_poly points
-
-
-let pentagon ?x ?y side_length =
-  let center = match (x, y) with
-    | Some x, Some y -> { x; y }
-    | _ -> canvas_mid in
-  Pentagon { c = center; side_length }
-
-let show shapes = List.iter render_shape shapes
-
 let () =
-  open_graph (" " ^ string_of_int (fst canvas_size) ^ "x" ^ string_of_int (snd canvas_size));
+  open_graph " 500x500";
+  
   set_color black;
 
-  let pent = pentagon 100 in
-  show [pent];
+  let draw_pentagon () =
+    let points = 
+      [|
+        (250, 450);
+        (60, 312);
+        (132, 88);
+        (368, 88);
+        (440, 312);
+      |] in
+    Graphics.draw_poly points
+  in
+
+  draw_pentagon();
 
   ignore (read_line ());
-  close_graph ()
+  close_graph ();
+  exit 0
