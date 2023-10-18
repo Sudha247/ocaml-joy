618e1fce40b2e8035c7348dd14701a852f318a01
Titilayo <tadeyemi457@gmail.com>
2023-10-18 13:13:42 +0100

    simplified circle grid code

--- examples/joy_circle_grid.ml
+++ examples/joy_circle_grid.ml
@@ -1,34 +1,30 @@
 open Joy.Shape
 
-let grid_of_circles canvas_width canvas_height spacing =
+let canvas_size = (500, 500)
+
+let grid_of_circles (canvas_width, canvas_height) spacing =
   let create_circle x y = circle 50 |> translate x y in
   let num_circles_x = canvas_width / spacing in
   let num_circles_y = canvas_height / spacing in
   let initial_x = (canvas_width - (num_circles_x * spacing)) / 2 in
   let initial_y = (canvas_height - (num_circles_y * spacing)) / 2 in
 
+
   let circles =
-    List.flatten
-      (List.map
-         (fun i ->
-           List.map
-             (fun j ->
-               create_circle
-                 (initial_x + (i * spacing))
-                 (initial_y + (j * spacing)))
-             (List.init num_circles_y (fun x -> x)))
-         (List.init num_circles_x (fun x -> x)))
-  in
+    List.flatten (List.init num_circles_x (fun i ->
+      List.init num_circles_y (fun j ->
+        let x_position = initial_x + (i * spacing) in
+        let y_position = initial_y + (j * spacing) in
+        create_circle x_position y_position
+      )
+    ))
+  in 
 
   show circles
 
 let () =
   init ();
-  let canvas_width = 800 in
-  let canvas_height = 600 in
-  let spacing = 150 in
-
-  grid_of_circles canvas_width canvas_height spacing;
-
+  grid_of_circles canvas_size 150;
   ignore (read_line ());
   close ()
+
