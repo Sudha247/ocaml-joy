d8c3fd1afc415911b700441b2fda8d1c86c7c3e7
Fay Carsons <faycarsons23@gmail.com>
2023-10-19 16:02:23 -0400

    added 'Complex' shape type

--- lib/shape.mli
+++ lib/shape.mli
@@ -5,6 +5,7 @@ val render_shape : shape -> unit
 val circle : ?x:int -> ?y:int -> int -> shape
 val rectangle : ?x:int -> ?y:int -> int -> int -> shape
 val ellipse : ?x:int -> ?y:int -> int -> int -> shape
+val complex : shape list -> shape
 
 val line : ?x1:int -> ?y1:int -> int -> int -> shape
 val translate: int -> int -> shape -> shape
