092e4fca736748ac2f1e07745e233746bea365ea
Mankavelda <manjongmanka@gmail.com>
2023-10-22 11:43:48 +0100

    fix: update semi-circle to use draw_arc

--- examples/semi_circle.ml
+++ examples/semi_circle.ml
@@ -1,31 +1,20 @@
 open Graphics
 
 let () =
-open_graph " 300x300";
-set_color black;
+  open_graph "300x300";
+  set_color black;
 
-let x = 150 in
-let y = 150 in
-let radius = 50 in
+  let x = 150 in
+  let y = 150 in
+  let radius = 50 in
 
-(* Draw a full circle *)
-draw_circle x y radius;
+  (* Specify the angle range for a semi-circle (0 to 180 degrees) *)
+  let start_angle = 0 in
+  let end_angle = 180 in
 
-(* Calculate the coordinates for the rectangle to erase the unwanted part *)
-let rect_x = x - radius in
-let rect_y = y in
-let rect_width = 2 * radius in
-let rect_height = radius in
+ 
+  draw_arc x y radius start_angle end_angle;
 
-(* Erase the upper part of the circle to create a semi-circle effect *)
-set_color Graphics.background;
-fill_rect rect_x rect_y rect_width rect_height;
-
-(* Reset the color to black for future drawing *)
-set_color black;
-
-ignore (read_line ());
-  close_graph (); 
+  ignore (read_line ());
+  close_graph ();
   exit 0
-
-
