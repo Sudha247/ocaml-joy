56ef2a7554baf4572248e1922ad04bddd3581174
Fay Carsons <faycarsons23@gmail.com>
2024-01-12 14:21:57 -0500

    Refactor Transform.compose to have more specific typing

--- lib/joy.mli
+++ lib/joy.mli
@@ -11,11 +11,19 @@ val ellipse : ?c:float point -> int -> int -> shape
 val line : ?a:float point -> float point -> shape
 val polygon : float point list -> shape
 val complex : shapes -> shape
+<<<<<<< HEAD
 val rotate : int -> transformation
 val translate : int -> int -> transformation
 val scale : float -> transformation
 val compose : transformation -> transformation -> transformation
 val repeat : int -> transformation -> transformation
+=======
+val rotate : int -> shape -> shape
+val translate : float -> float -> shape -> shape
+val scale : float -> shape -> shape
+val compose : (shape -> shape) -> (shape -> shape) -> shape -> shape
+val repeat : int -> (shape -> shape) -> shape -> shape
+>>>>>>> fa1c1cf (Refactor Transform.compose to have more specific typing)
 val context : Context.context option ref
 val set_color : int * int * int -> unit
 val background : int * int * int * int -> unit
