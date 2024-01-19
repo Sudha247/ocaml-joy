581fdd0af664350f479668343635eebb8e64130e
Fay Carsons <faycarsons23@gmail.com>
2024-01-18 22:00:30 -0500

    more merging ??

--- examples/repeat.ml
+++ examples/repeat.ml
@@ -10,16 +10,7 @@ open Joy
 
 let () =
   init ();
-<<<<<<< HEAD
   let circle = circle ~c:(point (-100) 0) 50 in
   let shapes = repeat 10 (translate 10 0) circle in
-=======
-  let circle = circle ~c:(point (-100.) 0.) 50. in
-  let shapes = repeat 10 (translate 10. 0.) circle in
-<<<<<<< HEAD
-=======
-  set_color (0., 0., 0.);
->>>>>>> 7f4ab94 (Fix scaling factor in scale transform (#88))
->>>>>>> eaf1693 (Fix scaling factor in scale transform (#88))
   render shapes;
   write ~filename:"repeat.png" ()
--- lib/shape.mli
+++ lib/shape.mli
@@ -24,6 +24,12 @@ val circle : ?c:float point -> int -> shape
 val rectangle : ?c:float point -> int -> int -> shape
 val ellipse : ?c:float point -> int -> int -> shape
 val complex : shape list -> shape
+<<<<<<< HEAD
 val line : ?a:float point -> float point -> shape
 val polygon : float point list -> shape
 val with_color : color -> shape -> shape
+=======
+val line : ?a:point -> point -> shape
+val polygon : point list -> shape
+val with_color : Color.color -> shape -> shape
+>>>>>>> 6df00cd (more merging ??)
