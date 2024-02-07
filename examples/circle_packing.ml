5ca226a8d7b78d60774fbe9bb79d81c279bedf75
Fay Carsons <faycarsons23@gmail.com>
2024-02-07 13:56:32 -0500

    refactor circle packing example

--- examples/circle_packing.ml
+++ examples/circle_packing.ml
@@ -1,13 +1,13 @@
 open Joy
 
 (* global constants // RNG initialization *)
-let w, h = (900., 900.)
+let w, h = (1200., 900.)
 let min_radius = 20.
 let max_radius = 150.
 let num_circles = 5_000
 let max_attempts = 100_000
 let shrink_factor = 0.85
-let _ = Stdlib.Random.self_init ()
+let _ = Random.self_init ()
 
 let palette =
   [
@@ -35,75 +35,59 @@ let distance (x1, y1) (x2, y2) =
   let dy = y2 -. y1 in
   sqrt ((dx *. dx) +. (dy *. dy))
 
+(* determines if two circles overlap *)
+let overlaps (p1, r1) (p2, r2) =
+  let dist = distance p1 p2 in
+  dist < r1 +. r2
+
 (* creates a random point within screen bounds *)
 let rand_point () =
-  (Stdlib.Random.float w -. (w /. 2.), Stdlib.Random.float h -. (h /. 2.))
+  (Random.float w -. (w /. 2.), Random.float h -. (h /. 2.))
 
 (* creates a circle with a random center point and radius *)
 let rand_circle () =
   let point = rand_point () in
-  (point, min_radius +. Stdlib.Random.float (max_radius -. min_radius))
+  (point, min_radius +. Random.float (max_radius -. min_radius))
 
 (* creates a lis of packed circles *)
 let pack_circles () =
-  (* determines if two circles overlap *)
-  let overlaps (p1, r1) (p2, r2) =
-    let dist = distance p1 p2 in
-    dist < r1 +. r2
-  in
   (* checks whether a circle intersects with a list of circles *)
-  let check_overlaps lst current =
+  let overlap_any lst current =
     List.fold_right (fun circle acc -> overlaps circle current || acc) lst false
   in
-  (* creates a new circle, checks if it intersects previous circles,
-     if max attempts have been reached,
-     or if the desired number of circles have been created.
-     From there it either recurses with or without the new circle,
-     or returns the list of circles *)
-  let rec pack lst attempts =
-    let new_circle = rand_circle () in
-    let does_overlap = check_overlaps lst new_circle in
-    let safe = List.length lst < num_circles - 1 && attempts < max_attempts in
-    match (does_overlap, safe) with
-    | true, true -> pack lst (attempts + 1)
-    | true, false -> new_circle :: lst
-    | _ -> pack (new_circle :: lst) attempts
+  let rec find_new old attempts = 
+    let new_circle = rand_circle () in 
+    if overlap_any old new_circle then 
+      find_new old (attempts + 1)
+    else
+      (new_circle :: old, attempts)
   in
-  let attempts = 0 in
-  let lst = [ rand_circle () ] in
-  pack lst attempts
+  let rec pack lst = function 
+    | n when n < max_attempts && List.length lst <= num_circles -> 
+      let circles, n = find_new lst n in 
+      pack circles (n + 1)
+    | _ -> 
+      lst
+  in 
+  pack [] 0
+
+(* Creates a Joy circle from the tuple representation used in packing *)
+let circle_from_repr ((x, y), r) = circle ~c:{x; y} (int_of_float r)
 
 (* pulls a random color from the 'palette' list
    sets draw color with it
    then draws circle *)
 let draw_with_color circle =
-  let idx = Stdlib.Random.full_int (List.length palette - 1) in
+  let idx = Random.full_int (List.length palette - 1) in
   set_color (List.nth palette idx);
   render circle
-
-(* turns a circle into a list of concentric circles *)
-let make_concentric circle =
-  let rec shrink lst =
-    let point, radius = List.hd (List.rev lst) in
-    if radius <= 1. then lst
-    else
-      let new_circle = (point, radius *. shrink_factor) in
-      shrink (lst @ [ new_circle ])
-  in
-  shrink [ circle ]
 
 (* main fn *)
 let () =
   init ~size:(int_of_float w, int_of_float h) ();
   background (255, 255, 255, 255);
   set_line_width 1;
-  let circles = pack_circles () in
-  let circles = List.flatten (List.map make_concentric circles) in
-  List.iter
-    (fun ((x, y), radius) ->
-      draw_with_color
-        (circle
-           ~c:(point (int_of_float x) (int_of_float y))
-           (int_of_float radius)))
-    circles;
+  let circles = pack_circles () |> List.map circle_from_repr in
+  let circles = List.map (repeat 8 (scale 0.9)) circles in 
+  List.iter draw_with_color circles;
   write ~filename:"Circle packing.png" ()
