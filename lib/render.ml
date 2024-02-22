5d8631d38fb6f5448b8ba91e78c9428c510110c9
Fay Carsons <faycarsons23@gmail.com>
2024-02-22 16:58:41 -0500

    fixing broken merge && properly closing paths when rendering shapes

--- lib/render.ml
+++ lib/render.ml
@@ -3,13 +3,6 @@ open Context
 
 let tmap f (x, y) = (f x, f y)
 
-let denormalize point =
-  let x, y = resolution () |> tmap float_of_int in
-  let canvas_mid = { x; y } /! 2. in
-  ((point.x +. canvas_mid.x) /. x, (point.y +. canvas_mid.y) /. y)
-
-let euclid_norm (x, y) = sqrt (Float.pow x 2. +. Float.pow y 2.) /. 2.
-
 let draw_circle ctx ({ c; radius; stroke; fill } : circle) =
   let stroke_circle stroke =
     set_color stroke;
@@ -19,20 +12,14 @@ let draw_circle ctx ({ c; radius; stroke; fill } : circle) =
     set_color fill;
     Cairo.fill_preserve ctx.ctx
   in
-  let size = tmap float_of_int ctx.size in
-  let x, y = denormalize c in
-  let radius = radius /. euclid_norm size in
-  save ();
-  Cairo.arc ctx.ctx x y ~r:radius ~a1:0. ~a2:(Float.pi *. 2.);
+  Cairo.arc ctx.ctx c.x c.y ~r:radius ~a1:0. ~a2:(Float.pi *. 2.);
   Option.iter stroke_circle stroke;
   Option.iter fill_circle fill;
-  restore ()
+  Cairo.Path.clear ctx.ctx
 
-let create_control_points (c, rx, ry) =
-  let size = resolution () |> tmap float_of_int in
-  let x, y = denormalize c in
-  let half_height = ry /. snd size in
-  let width_two_thirds = rx /. fst size *. (2. /. 3.) *. 2. in
+let create_control_points ({x; y}, rx, ry) =
+  let half_height = ry /. 2. in
+  let width_two_thirds = rx *. (2. /. 3.) *. 2. in
   ( { x; y = y -. half_height },
     ( x +. width_two_thirds,
       y -. half_height,
@@ -56,7 +43,6 @@ let draw_ellipse ctx { c; rx; ry; stroke; fill } =
     set_color fill;
     Cairo.fill_preserve ctx.ctx
   in
-  save ();
   let start, curve_one, curve_two = create_control_points (c, rx, ry) in
   Cairo.move_to ctx.ctx start.x start.y;
   let x1, y1, x2, y2, x3, y3 = curve_one in
@@ -65,15 +51,14 @@ let draw_ellipse ctx { c; rx; ry; stroke; fill } =
   Cairo.curve_to ctx.ctx x1 y1 x2 y2 x3 y3;
   Option.iter stroke_ellipse stroke;
   Option.iter fill_ellipse fill;
-  restore ()
+  Cairo.Path.clear ctx.ctx
 
 let draw_line ctx { a; b; stroke } =
-  save ();
-  let x1, y1 = denormalize a in
-  let x2, y2 = denormalize b in
   set_color stroke;
-  Cairo.move_to ctx.ctx x1 y1;
-  Cairo.line_to ctx.ctx x2 y2;
+  let { x; y } = a in
+  Cairo.move_to ctx.ctx x y;
+  let { x; y } = b in
+  Cairo.line_to ctx.ctx x y;
   Cairo.stroke ctx.ctx
 
 let rec take n lst =
@@ -84,7 +69,7 @@ let rec take n lst =
       let taken, rest = take (n - 1) xs in
       (x :: taken, rest)
 
-let rec partition n ?step lst =
+let rec partition n ?(step = 0) lst =
   match lst with
   | [] -> []
   | _ ->
@@ -92,9 +77,7 @@ let rec partition n ?step lst =
       if List.length taken = n then
         taken
         ::
-        (match step with
-        | Some s -> partition n ~step:s (List.tl lst)
-        | None -> partition n ~step:0 (List.tl lst))
+        partition n ~step (List.tl lst)
       else []
 
 let draw_polygon ctx { vertices = points; stroke; fill } =
@@ -107,7 +90,6 @@ let draw_polygon ctx { vertices = points; stroke; fill } =
     Cairo.fill_preserve ctx.ctx
   in
   let points = partition 2 ~step:1 (points @ [ List.hd points ]) in
-  save ();
   List.iter
     (fun pair ->
       let { x = x1; y = y1 }, { x = x2; y = y2 } =
@@ -118,7 +100,7 @@ let draw_polygon ctx { vertices = points; stroke; fill } =
     points;
   Option.iter stroke_rect stroke;
   Option.iter fill_rect fill;
-  restore ()
+  Cairo.Path.clear ctx.ctx
 
 let rec render_shape ctx = function
   | Circle circle -> draw_circle ctx circle
@@ -137,11 +119,8 @@ let show shapes =
   | None -> fail ()
 
 let render_axes () =
-  print_endline "rendering axes!";
-  save ();
   let x, y = resolution () |> tmap float_of_int in
   let half_x, half_y = (x /. 2., y /. 2.) in
   let x_axis = line ~a:{ x = 0.; y = -.half_y } { x = 0.; y = half_y } in
   let y_axis = line ~a:{ x = -.half_x; y = 0. } { x = half_x; y = 0. } in
-  set_color (0, 0, 0);
   show [ x_axis; y_axis ]
