685383dc7090e3b4edf5a5a6659e4a04b8231839
Fay Carsons <faycarsons23@gmail.com>
2024-02-22 20:02:31 -0500

    optimization, add pareto scaling

--- examples/circle_packing.ml
+++ examples/circle_packing.ml
@@ -2,8 +2,10 @@ open Joy
 
 (* global constants // RNG initialization *)
 let size = (800, 800)
-let min_radius = 20.
-let max_radius = 150.
+let min_radius = 10.
+let max_radius = 200.
+let pareto_scale = 0.99
+let pareto_alpha = 0.1
 
 (* Maximum number of circles *)
 let num_circles = 5000
@@ -37,6 +39,17 @@ let palette =
 let tmap f (a, b) = (f a, f b)
 let rand_nth coll = List.length coll |> Random.full_int |> List.nth coll
 
+(* Pareto distribution float random for radii *)
+let pareto scl alpha =
+  let x = Random.float (1. -. Float.epsilon) +. Float.epsilon in
+  scl *. ((1. -. x) ** (-1. /. alpha))
+
+(* Clamp floats to [min..max] *)
+let clamp min max = function
+  | n when n >= max -> max
+  | n when n <= min -> min
+  | n -> n
+
 (* distance between two points *)
 let distance (x1, y1) (x2, y2) =
   let dx = x2 -. x1 in
@@ -56,32 +69,30 @@ let rand_point () =
 (* Creates a circle with a random center point and radius *)
 let rand_circle () =
   let x, y = rand_point () in
-  (x, y, min_radius +. Random.float (max_radius -. min_radius))
+  (x, y, pareto pareto_scale pareto_alpha |> clamp min_radius max_radius)
 
 (* Creates a list of packed(non-ovelapping) circles *)
 let pack_circles () =
   (* checks whether a circle intersects with a list of circles *)
-  let overlap_any lst current =
-    List.fold_right (fun circle acc -> overlaps circle current || acc) lst false
+  let rec overlap_any current = function
+    | [] -> false
+    | x :: xs -> overlaps current x || overlap_any current xs
   in
-  (* Atttempts to create a circle that meets all the necessary conditions, 
+  (* Atttempts to create a circle that meets all the necessary conditions,
      recurses with list of circles *)
-  let rec pack n lst = 
-    if n >= max_attempts || List.length lst >= num_circles then 
-      lst 
+  let rec pack n lst =
+    if n >= max_attempts || List.length lst >= num_circles then lst
     else
-      let new_circle = rand_circle () in 
-      if overlap_any lst new_circle then 
-        pack (n + 1) lst 
-      else
-        pack (n + 1) (new_circle :: lst)  
+      let new_circle = rand_circle () in
+      if overlap_any new_circle lst then pack (n + 1) lst
+      else pack (n + 1) (new_circle :: lst)
   in
-  pack 0 [] 
+  pack 0 []
 
 (* converts a circle into a list of concentric circles *)
 let shrink c =
   let rec shrink' = function
-    | (x, y, r) :: _ as lst when r > 1. ->
+    | (x, y, r) :: _ as lst when r > 2. ->
         shrink' ((x, y, r *. shrink_factor) :: lst)
     | lst -> lst
   in
@@ -96,15 +107,15 @@ let () =
   init ~size ();
   (* Create packed circles *)
   pack_circles ()
-    (* Convert them to lists of concentric circles *)
-    |> List.map shrink
-    (* Flatten the list of circle lists *)
-    |> List.flatten
-    (* Convert from our tuple representation to the Joy representation *)
-    |> List.map circle_from_repr
-    (* Add a stroke color *)
-    |> List.map (fun c -> with_stroke (rand_nth palette) c)
-    (* Render them! *)
-    |> show;
+  (* Convert them to lists of concentric circles *)
+  |> List.map shrink
+  (* Flatten the list of circle lists *)
+  |> List.flatten
+  (* Convert from our tuple representation to the Joy representation *)
+  |> List.map circle_from_repr
+  (* Add a stroke color *)
+  |> List.map (fun c -> with_stroke (rand_nth palette) c)
+  (* Render them! *)
+  |> show;
   (* Write to PNG *)
   write ~filename:"circle-packing.png" ()
