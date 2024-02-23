0199c64493ff7aff5ad7c13da0eda3859699e34d
Fay Carsons <faycarsons23@gmail.com>
2024-02-23 11:36:48 -0500

    fixing line_width conversion

--- lib/joy.ml
+++ lib/joy.ml
@@ -37,7 +37,7 @@ let set_line_width = Context.set_line_width
 let init ?(background = Color.white) ?(line_width = 2) ?(size = (500, 500))
     ?(axes = false) () =
   Context.init_context (Color.opaque background)
-    (float_of_int line_width /. 1000.)
+    (float_of_int line_width)
     size axes
 
 let write ?(filename = "joy.png") () =
