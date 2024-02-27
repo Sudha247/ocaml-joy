e825be5ed0280fcdc651831c180e5faa1eae0ead
Fay Carsons <faycarsons23@gmail.com>
2024-02-27 14:53:33 -0500

    refactor quadtree.ml to use new API

--- examples/quadtree.ml
+++ examples/quadtree.ml
@@ -5,6 +5,10 @@ let size = 800.
 let half_size = size /. 2.
 let max_leaf_points = 4
 let clusters = 32
+let point_size  = 1
+
+let box_color = (0, 0, 0)
+let point_color = (255, 1, 1)
 
 (* Init rng *)
 let _ = Random.self_init ()
@@ -25,24 +29,28 @@ let ( /! ) ({ x; y } : point) scalar : point =
 let rand_point () : point =
   { x = Random.float size -. half_size; y = Random.float size -. half_size }
 
-let centered_point (center : point) _ : point =
+(* Creates a point within 50 units of a center *)
+let centered_point (center : point) _: point =
   let offset () = Random.float 100. -. 50. in
   center +~ { x = offset (); y = offset () }
 
+(* Creates a list of random points clustered around a center point *)
 let cluster _ =
   let center = rand_point () in
   List.init (8 + Random.int 24) (centered_point center)
 
 (* Box and utils *)
+
+(* Axis aligned bounding box *)
 type box = { min : point; max : point }
-(** Axis aligned bounding box *)
+
 
 let box min max = { min; max }
 
-(** Returns the middle point of the box *)
+(* Returns the middle point of the box *)
 let midpoint { min; max } = pmap2 ( +. ) min max /! 2.
 
-(** Subdivides a box into four even axis-aligned boxes *)
+(* Subdivides a box into four even axis-aligned boxes *)
 let quarters ({ min; max } as box) =
   let mid = midpoint box in
   let lu = { min = { x = min.x; y = mid.y }; max = { x = mid.x; y = max.y } } in
@@ -51,18 +59,18 @@ let quarters ({ min; max } as box) =
   let ld = { min; max = mid } in
   (lu, ru, rd, ld)
 
-(** checks whether point is within bounds of box *)
-let contains { min; max } ({ x; y } : point) =
+(* Checks whether point is within bounds of box *)
+let contains { min; max }  ({ x; y }: point) =
   x > min.x && x < max.x && y > min.y && y < max.y
 
 (* Quadtree and utils *)
+
+(* 2-tuple of bounding box * 'a list of elts whose positions are within that box *)
 type 'a leaf = box * 'a list
-(** Leaf is 2-tuple of bounding box * 'a list of elts whose position is within that box *)
 
 type 'a tree = Leaf of 'a leaf | Node of 'a tree list
-(* Node potentially doesn't need to hold aabb? *)
 
-(** Constructs tree from root *)
+(* Constructs tree from root *)
 let split_root box points =
   (* Groups points with the boxes that contain them *)
   let partition (lu, ru, rd, ld) es =
@@ -83,44 +91,37 @@ let split_root box points =
   in
   split (box, points)
 
+(* Builds our float point tree *)
 let build () =
   let root = box (splat (-.half_size)) (splat half_size) in
   let points = List.flatten (List.init clusters cluster) in
   split_root root points
 
+(* Converts our constructed tree into a flat list of shapes for rendering *)
 let to_flat_shapes tree =
+  let open Joy in 
+  (* Converts box into rectangle *)
   let rect_of_bb bb =
-    Joy.rectangle ~c:(midpoint bb)
+    rectangle ~c:(midpoint bb)
       (int_of_float (bb.max.x -. bb.min.x))
       (int_of_float (bb.max.y -. bb.min.y))
+    |> with_stroke box_color
   in
-  let circle_of_point pt = Joy.circle ~c:pt 1 in
+  (* Converts point into circle of radius 1 *)
+  let circle_of_point pt = circle ~c:pt point_size |> with_stroke point_color in
+  (* Traverses tree recursively *)
   let rec convert xs = function
-    | Node children -> List.flatten (List.map (convert xs) children)
+    | Node children -> List.concat_map (convert xs) children
     | Leaf (aabb, es) ->
         let b = rect_of_bb aabb in
         List.map circle_of_point es @ (b :: xs)
   in
   convert [] tree
 
-(* With color handling system this function won't be necessary as color can be
-   decided at construction *)
-let render_color shape =
-  let open Joy in
-  match shape with
-  | Shape.Circle _ ->
-      set_color (255, 1, 1);
-      render shape
-  | _ ->
-      set_color (0, 0, 0);
-      render shape
-
 let () =
   let open Joy in
   init ();
-  background (255, 255, 255, 255);
   let tree = build () in
-  let to_shapes = to_flat_shapes tree in
-  set_color (0, 0, 0);
-  List.iter render_color to_shapes;
+  let shapes = to_flat_shapes tree in
+  show shapes;
   write ~filename:"quadtree.png" ()
