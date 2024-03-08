cca0c7de9366b903578c34ec9e127b2c9931a76b
Fay Carsons <faycarsons23@gmail.com>
2024-03-08 10:58:56 -0500

    refactor fill-rect example

--- examples/fill_rect.ml
+++ examples/fill_rect.ml
@@ -2,6 +2,6 @@ open Joy
 
 let () =
   init ();
-  let r = rectangle 500 500 |> no_stroke |> with_fill (255, 0, 0) in
+  let r = rectangle 200 200 |> with_fill (255, 0, 0) in
   show [ r ];
   write ~filename:"fill-rect.png" ()
